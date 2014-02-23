d486087485844c11ff298939a0144735f0820478
toshipon <toshipon123@gmail.com>
2014-02-23 16:20:32 +0900

    Replace new boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seichijunrei::Application.config.secret_key_base = 'afd545f6db27ce383c75d295c1a1714a64b5cf8688e4094dad47f7d95e67ad219c702234eb72664c383adf6de4e9079d76e43698fe1684c791ae33b46536e801'
